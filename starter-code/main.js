09b046b71cc4c00d8e74f1ec117c0e637f1eed48
Jason Lai <JLMacAir@Jasons-Air.lan>
2016-06-04 21:10:54 +0100

    Unit 9 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,28 @@ var cardOne = "Queen"
 var cardTwo = "King"
 var cardThree = "Queen"
 var cardFour = "King"
+
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+
+else {
+	alert("Sorry try again.");
+}
+
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+
+else {
+	alert("Sorry try again.");
+}
+
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
+
+else {
+	alert("Sorry try again")
+}
